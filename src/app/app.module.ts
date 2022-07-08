1943192034355a86bbce84c1ecb5540e66702fcd
abhxi-r-s <abhishekunni8281@gmail.com>
2022-07-08 10:47:22 +0530

    student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponentComponent } from './student-component/student-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponentComponent
   ],
   imports: [
     BrowserModule,
